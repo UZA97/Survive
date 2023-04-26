187e5e7a9e36efa39ddf5e972a321ac57f955644
UZA97 <woocfgzn@naver.com>
2023-04-26 12:18:47 +0900

    Add SPUM(Player)

--- Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -6,11 +6,23 @@ public class JoystickPlayerExample : MonoBehaviour
 {
     public float speed;
     public VariableJoystick variableJoystick;
-    public Rigidbody rb;
 
     public void FixedUpdate()
     {
-        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        Vector3 direction = Vector3.up * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        this.transform.position += direction*speed;
+        //rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+        //Vector2 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        //this.transform.position = direction;
+    }
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+
     }
 }
